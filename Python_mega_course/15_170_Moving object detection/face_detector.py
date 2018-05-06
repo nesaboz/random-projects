803a6d5936cdb736c305f923561763dc7dbc1d1b
nenad.bozinovic <nesaboz@gmail.com>
2018-05-05 19:09:57 -0700

    adding face recognition

--- Python_mega_course/15_170_Moving object detection/face_detector.py
+++ Python_mega_course/15_170_Moving object detection/face_detector.py
@@ -6,21 +6,22 @@ from matplotlib.patches import Rectangle
 # from https://github.com/opencv/opencv/tree/master/data/haarcascades
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img = cv2.imread("news.jpg")
+img = cv2.imread("Nenad.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5)
 for x, y, w, h in faces:
     print(x)
     img = cv2.rectangle(img, (x,y), (x + w, y + h), (0, 255, 0), 3)
     # input("Press Enter to continue...")
 
 
-# resized = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
-cv2.imshow("Gray", img)
+resized = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
+cv2.imshow("Gray", resized)
 # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 cv2.waitKey(0)
+# cv2.waitKey(2000)
 # cv2.destroyAllWindows()
 
 
